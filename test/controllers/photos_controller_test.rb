ee7abe0d3bdd3631d6ebecf3025e709dbf4dcfbb
atshaw1994 <atshaw1994@gmail.com>
2025-09-03 18:46:01 -0400

    Update test suite

--- test/controllers/photos_controller_test.rb
+++ test/controllers/photos_controller_test.rb
@@ -2,17 +2,19 @@ require "test_helper"
 
 class PhotosControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get photos_index_url
+    get photos_path
     assert_response :success
   end
 
   test "should get new" do
-    get photos_new_url
+    get new_photo_path
     assert_response :success
   end
 
   test "should get create" do
-    get photos_create_url
-    assert_response :success
+    assert_difference("Photo.count") do
+      post photos_path, params: { photo: { title: "Test Photo", image: fixture_file_upload("files/photo.jpg", "image/jpeg") } }
+    end
+    assert_redirected_to photos_path
   end
 end
